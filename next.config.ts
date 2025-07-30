e8a943cc738e3968fdf465e7435b33aa04fed965
Raul Vargas <rvargas38@ucmerced.edu>
2025-07-30 11:25:55 -0700

    Remove static export mode for proper Vercel rendering

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: isProd ? '/vargas_dev' : '',
-  assetPrefix: isProd ? '/vargas_dev/' : '',
+
 };
 
 module.exports = nextConfig;
